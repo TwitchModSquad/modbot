0f5ef7fb1f5cc2c889003d07f0060e9c9537ffa1
Tyler Twining <twijn@twijn.net>
2022-10-12 19:00:58 -0400

    Fix threading on updateChatIndexes

--- interval/updateChatIndexs.js
+++ interval/updateChatIndexs.js
@@ -1,26 +1,30 @@
 const con = require("../autodatabase");
 
 module.exports = () => {
-    con.query("select streamer_id, count(streamer_id) as chat_count from twitch__chat group by streamer_id;", (err, res) => {
+    con.query("select streamer_id, count(streamer_id) as chat_count from twitch__chat group by streamer_id;", async (err, res) => {
         if (err) global.api.Logger.warning(err);
         
-        res.forEach(row => {
-            con.query("insert into twitch__chat_streamers (streamer_id, chat_count) values (?, ?) on duplicate key update chat_count = ?;", [row.streamer_id, row.chat_count, row.chat_count], err => {
-                if (err) global.api.Logger.warning(err);
-            });
-        });
+        for (let i = 0; i < res.length; i++) {
+            try {
+                await con.pquery("insert into twitch__chat_streamers (streamer_id, chat_count) values (?, ?) on duplicate key update chat_count = ?;", [row.streamer_id, row.chat_count, row.chat_count]);
+            } catch(err) {
+                global.api.Logger.warning(err);
+            }
+        }
 
         global.api.Logger.info("Completed twitch__chat_streamers index");
     });
 
-    con.query("select user_id, streamer_id, count(streamer_id) as chat_count, max(timesent) as last_active from twitch__chat group by streamer_id, user_id;", (err, res) => {
+    con.query("select user_id, streamer_id, count(streamer_id) as chat_count, max(timesent) as last_active from twitch__chat group by streamer_id, user_id;", async (err, res) => {
         if (err) global.api.Logger.warning(err);
         
-        res.forEach(row => {
-            con.query("insert into twitch__chat_chatters (chatter_id, streamer_id, chat_count, last_active) values (?, ?, ?, ?) on duplicate key update chat_count = ?, last_active = ?;", [row.user_id, row.streamer_id, row.chat_count, row.last_active, row.chat_count, row.last_active], err => {
-                if (err) global.api.Logger.warning(err);
-            });
-        });
+        for (let i = 0; i < res.length; i++) {
+            try {
+                await con.pquery("insert into twitch__chat_chatters (chatter_id, streamer_id, chat_count, last_active) values (?, ?, ?, ?) on duplicate key update chat_count = ?, last_active = ?;", [row.streamer_id, row.chat_count, row.chat_count]);
+            } catch(err) {
+                global.api.Logger.warning(err);
+            }
+        }
 
         global.api.Logger.info("Completed twitch__chat_chatters index");
     });
