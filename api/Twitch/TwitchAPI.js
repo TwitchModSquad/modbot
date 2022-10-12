d181397b92ed2befd232dac65ff2b1f4a174a3db
Tyler Twining <twijn@twijn.net>
2022-10-12 16:45:20 -0400

    Add docs & getBans method

--- api/Twitch/TwitchAPI.js
+++ api/Twitch/TwitchAPI.js
@@ -6,7 +6,7 @@ const config = require("../../config.json");
  */
 class TwitchAPI {
     /**
-     * Bans a user in the specified channel utilizing an access token
+     * Bans a user in the specified channel utilizing an access token of the moderator or broadcaster
      * @param {number} broadcaster_id 
      * @param {number} moderator_id 
      * @param {string} access_token 
@@ -30,6 +30,13 @@ class TwitchAPI {
         });
     }
 
+    /**
+     * Adds user_id as a moderator in broadcaster_id using the broadcaster's access token
+     * @param {number} broadcaster_id 
+     * @param {number} user_id 
+     * @param {string} access_token 
+     * @returns {Promise<void>}
+     */
     modUser(broadcaster_id, user_id, access_token) {
         return new Promise(async (resolve, reject) => {
             const oauthResult = await fetch(`https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=${encodeURIComponent(broadcaster_id)}&user_id=${encodeURIComponent(user_id)}`, {
@@ -45,6 +52,33 @@ class TwitchAPI {
             } else {
                 reject(oauthResult.status + " " + oauthResult.statusText);
             }
+        });
+    }
+    
+    /**
+     * Gets ban information of user_id in broadcaster_id with the specified broadcaster access_token
+     * @param {number} broadcaster_id 
+     * @param {number} user_id 
+     * @param {string} access_token 
+     * @returns {Promise<any>}
+     */
+    getBans(broadcaster_id, user_id, access_token) {
+        return new Promise(async (resolve, reject) => {
+            const oauthResult = await fetch(`https://api.twitch.tv/helix/moderation/banned?broadcaster_id=${encodeURIComponent(broadcaster_id)}&user_id=${encodeURIComponent(user_id)}`, {
+                method: 'GET',
+                headers: {
+                    Authorization: "Bearer " + access_token,
+                    "Client-Id": config.twitch.client_id,
+                },
+            });
+        
+            oauthResult.json().then(e => {
+                if (e?.data) {
+                    resolve(e.data);
+                } else {
+                    reject("Failed to get ban data: " + e?.message);
+                }
+            }, reject);
         });
     }
 }
