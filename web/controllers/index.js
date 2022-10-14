300caaad06d80500cb64ed60bb534cdc0aefb01b
Tyler Twining <twijn@twijn.net>
2022-10-14 10:30:37 -0400

    Add /login redirect

--- web/controllers/index.js
+++ web/controllers/index.js
@@ -8,6 +8,10 @@ const panel = require("./panel/");
 authRouter.use("/signon", signon);
 authRouter.use("/panel", panel)
 
+authRouter.use("/login", (req, res) => {
+    res.redirect("/signon");
+})
+
 noAuthRouter.get("/", (req, res) => {
     res.render("pages/index")
 });
