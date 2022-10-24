fb4c81cff371f8098747d5f01f2c861cc2022245
Tyler Twining <twijn@twijn.net>
2022-10-23 22:09:43 -0400

    Layout for ban automation creation

--- web/controllers/panel/automation/ban.js
+++ web/controllers/panel/automation/ban.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const api = require("../../../../api/");
 
-router.get("/list", (req, res) => {
+router.get("/list", async (req, res) => {
+    try {
+        let data = {
+            session: req.session,
+            automations: await api.Automation.getBanAutomationsByCreator(req.session.identity.id),
+        };
+
+        res.render("pages/panel/automation/ban/list", data);
+    } catch(err) {
+        api.Logger.severe(err);
+        res.send("An error occurred. Report this to an administrator");
+    }
+});
+
+router.get("/create", async (req, res) => {
     let data = {
         session: req.session,
     };
 
-    res.render("pages/panel/automation/ban/list", data);
+    res.render("pages/panel/automation/ban/create", data);
+});
+
+router.post("/create", async (req, res) => {
+    res.send("This is where I'd process that request, but I'm too lazy currently.")
 });
 
 module.exports = router;
