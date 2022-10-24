aa27c4bfd6009be4911c2ff31467d9285fee3743
Tyler Twining <twijn@twijn.net>
2022-10-23 22:08:47 -0400

    Add legacy redirect for old user URIs

--- web/controllers/index.js
+++ web/controllers/index.js
@@ -5,6 +5,8 @@ const authRouter = express.Router();
 const signon = require("./signon/");
 const panel = require("./panel/");
 
+const api = require("../../api/");
+
 authRouter.use("/signon", signon);
 authRouter.use("/panel", panel)
 
@@ -14,6 +16,27 @@ authRouter.use("/login", (req, res) => {
 
 noAuthRouter.get("/", (req, res) => {
     res.render("pages/index")
+});
+
+const userRedirect = (req, res) => {
+    res.redirect("/panel/user/" + req.params.user);
+}
+
+noAuthRouter.get("/t/:user", userRedirect);
+noAuthRouter.get("/d/:user", userRedirect);
+noAuthRouter.get("/i/:user", (req, res) => {
+    api.getFullIdentity(req.params.user).then(identity => {
+        if (identity.twitchAccounts.length > 0) {
+            res.redirect("/panel/user/" + identity.twitchAccounts[0].id);
+        } else if (identity.discordAccounts.length > 0) {
+            res.redirect("/panel/user/" + identity.discordAccounts[0].id);
+        } else {
+            res.send("Invalid identity ID");
+        }
+    }, err => {
+        api.Logger.warning(err);
+        res.send("Invalid identity ID");
+    });
 });
 
 module.exports = {
