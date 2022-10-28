d798a5d855578015faa7bb218ae414650c88f4ba
Tyler Twining <twijn@twijn.net>
2022-10-28 18:18:41 -0400

    Add rules & targets in BanAutomation object

--- api/Automation/BanAutomation/Rule.js
+++ api/Automation/BanAutomation/Rule.js
@@ -15,13 +15,22 @@ class Rule {
     value;
 
     /**
+     * The user represented by this rule, if available
+     * @type {TwitchUser?}
+     */
+    user;
+
+    /**
      * Returns the Twitch user of this rule, if the type is "streamer" or "moderator"
      * @return {Promise<TwitchUser>}
      */
     getUser() {
         return new Promise((resolve, reject) => {
             if (type === "streamer" || type === "moderator") {
-                global.api.Twitch.getUserById(this.value).then(resolve, reject);
+                global.api.Twitch.getUserById(this.value).then(user => {
+                    this.user = user;
+                    resolve(user);
+                }, reject);
             } else {
                 reject("Rule type must be 'streamer' or 'moderator'");
             }
@@ -32,10 +41,12 @@ class Rule {
      * 
      * @param {"streamer"|"moderator"|"reason"|"chatlog"} type 
      * @param {string} value 
+     * @param {TwitchUser?} user 
      */
-    constructor(type, value) {
+    constructor(type, value, user = null) {
         this.type = type;
         this.value = value;
+        this.user = user;
     }
 
 }
--- api/Automation/BanAutomation/index.js
+++ api/Automation/BanAutomation/index.js
@@ -2,6 +2,8 @@ const FullIdentity = require("../../FullIdentity");
 const TwitchUser = require("../../Twitch/TwitchUser");
 const Rule = require("./Rule");
 
+const con = require("../../../database");
+
 class BanAutomation {
     /**
      * Ban automation ID
@@ -47,6 +49,115 @@ class BanAutomation {
         this.creator = creator;
         this.targets = targets;
         this.rules = rules;
+    }
+
+    /**
+     * Adds a target to this ban automation
+     * @param {TwitchUser} user 
+     * @returns {Promise<void>}
+     */
+    addTarget(user) {
+        return new Promise((resolve, reject) => {
+            con.query("insert into twitch__ban__automation_rules (automation_id, type, value) values (?, 'target', ?);", [this.id, user.id], err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    this.targets = [
+                        ...this.targets,
+                        user,
+                    ]
+                    resolve();
+                }
+            });
+        });
+    }
+
+    /**
+     * Adds a new "streamer" rule
+     * @param {TwitchUser} user 
+     * @returns {Promise<Rule>}
+     */
+    addRuleStreamer(user) {
+        return new Promise((resolve, reject) => {
+            con.query("insert into twitch__ban__automation_rules (automation_id, type, value) values (?, 'streamer', ?);", [this.id, user.id], err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    let rule = new Rule("streamer", user.id, user);
+                    this.rules = [
+                        ...this.rules,
+                        rule,
+                    ]
+                    resolve(rule);
+                }
+            });
+        });
+    }
+
+    /**
+     * Adds a new "moderator" rule
+     * @param {TwitchUser} user 
+     * @returns {Promise<Rule>}
+     */
+    addRuleModerator(user) {
+        return new Promise((resolve, reject) => {
+            con.query("insert into twitch__ban__automation_rules (automation_id, type, value) values (?, 'moderator', ?);", [this.id, user.id], err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    let rule = new Rule("moderator", user.id, user);
+                    this.rules = [
+                        ...this.rules,
+                        rule,
+                    ]
+                    resolve(rule);
+                }
+            });
+        });
+    }
+
+    /**
+     * Adds a new "chat log" rule
+     * @param {string} value 
+     * @returns {Promise<Rule>}
+     */
+    addRuleChatLog(value) {
+        return new Promise((resolve, reject) => {
+            con.query("insert into twitch__ban__automation_rules (automation_id, type, value) values (?, 'chatlog', ?);", [this.id, value], err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    let rule = new Rule("chatlog", value);
+                    this.rules = [
+                        ...this.rules,
+                        rule,
+                    ]
+                    resolve(rule);
+                }
+            });
+        });
+    }
+
+    /**
+     * Adds a new "reason" rule
+     * @param {string} value 
+     * @returns {Promise<Rule>}
+     */
+    addRuleReason(value) {
+        return new Promise((resolve, reject) => {
+            con.query("insert into twitch__ban__automation_rules (automation_id, type, value) values (?, 'reason', ?);", [this.id, value], err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    let rule = new Rule("reason", value);
+                    this.rules = [
+                        ...this.rules,
+                        rule,
+                    ]
+                    resolve(rule);
+                }
+            });
+        });
     }
 }
 
