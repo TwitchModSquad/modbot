9a7547831c7aff2a54702185f17f3a5391a19ce4
Tyler Twining <twijn@twijn.net>
2023-04-05 19:03:38 -0400

    Cleanup profile generator

--- discord/listeners/userProfileGenerator.js
+++ discord/listeners/userProfileGenerator.js
@@ -14,52 +14,6 @@ const notFoundEmbed = query => {
             .setColor(0x772ce8);
 }
 
-const process = async (content, regex, type) => {
-    let match = content.match(regex);
-    
-    if (match.length > 0) {
-        let embeds = [];
-        let errorEmbeds = [];
-
-        for (let i = 0; i < match.length; i++) {
-            try {
-                let obj;
-                if (type === "identity") {
-                    obj = [await (await api.getFullIdentity(match[i])).discordEmbed()];
-                } else if (type === "twitch") {
-                    obj = await api.Twitch.getUserByName(match[i], true);
-                    for (let oi = 0; oi < obj.length; oi++) {
-                        obj[oi] = await obj[oi].discordEmbed();
-                    }
-                    if (obj.length === 0) continue;
-                } else if (type === "discord") {
-                    obj = [await (await api.Discord.getUserById(match[i], false, true)).discordEmbed()];
-                }
-                embeds = [
-                    ...embeds,
-                    ...obj,
-                ];
-            } catch (err) {
-                global.api.Logger.warning(err);
-                errorEmbeds = [
-                    ...embeds,
-                    notFoundEmbed(match[i])
-                ];
-            }
-        }
-
-        return {
-            embeds: embeds,
-            errorEmbeds: errorEmbeds
-        };
-    }
-
-    return {
-        embeds: [],
-        errorEmbeds: []
-    };
-}
-
 const listener = {
     name: 'userProfileGenerator',
     eventName: 'messageCreate',
@@ -75,38 +29,47 @@ const listener = {
         let embeds = [];
         let errorEmbeds = [];
 
-        if (content.indexOf("-i:") !== -1) {
-            const result = await process(content, identityRegex, "identity");
-            embeds = [
-                ...embeds,
-                ...result.embeds,
-            ];
-            errorEmbeds = [
-                ...errorEmbeds,
-                ...result.errorEmbeds,
-            ];
+        const process = async (content, regex, type) => {
+            let match = content.match(regex);
+            
+            if (match.length > 0) {
+                for (let i = 0; i < match.length; i++) {
+                    try {
+                        let obj;
+                        if (type === "identity") {
+                            obj = [await (await api.getFullIdentity(match[i])).discordEmbed()];
+                        } else if (type === "twitch") {
+                            obj = await api.Twitch.getUserByName(match[i], true);
+                            for (let oi = 0; oi < obj.length; oi++) {
+                                obj[oi] = await obj[oi].discordEmbed();
+                            }
+                            if (obj.length === 0) continue;
+                        } else if (type === "discord") {
+                            obj = [await (await api.Discord.getUserById(match[i], false, true)).discordEmbed()];
+                        }
+                        embeds = [
+                            ...embeds,
+                            ...obj,
+                        ];
+                    } catch (err) {
+                        global.api.Logger.warning(err);
+                        errorEmbeds = [
+                            ...embeds,
+                            notFoundEmbed(match[i])
+                        ];
+                    }
+                }
+            }
         }
-        if (content.indexOf("-d:") !== -1) {
-            const result = await process(content, discordRegex, "discord");
-            embeds = [
-                ...embeds,
-                ...result.embeds,
-            ];
-            errorEmbeds = [
-                ...errorEmbeds,
-                ...result.errorEmbeds,
-            ];
+
+        if (content.indexOf("-i:") !== -1) {
+            await process(content, identityRegex, "identity");
         }
         if (content.indexOf("-t:") !== -1) {
-            const result = await process(content, twitchRegex, "twitch");
-            embeds = [
-                ...embeds,
-                ...result.embeds,
-            ];
-            errorEmbeds = [
-                ...errorEmbeds,
-                ...result.errorEmbeds,
-            ];
+            await process(content, twitchRegex, "twitch");
+        }
+        if (content.indexOf("-d:") !== -1) {
+            await process(content, discordRegex, "discord");
         }
 
         if (embeds.length > 0)
