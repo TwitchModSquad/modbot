7487c8cb86bbdd0f6668e892e8734b6ed7b00a05
Tyler Twining <twijn@twijn.net>
2023-04-25 12:03:42 -0400

    Start modcomment subcommands

--- discord/commands/modcomment.js
+++ discord/commands/modcomment.js
@@ -1,5 +1,5 @@
-const { EmbedBuilder, StringSelectMenuBuilder, ActionRowBuilder, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandStringOption } = require("discord.js");
-const games = require("../games");
+const { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandStringOption } = require("discord.js");
+const api = require("../../api/");
 
 const command = {
     data: new SlashCommandBuilder()
@@ -14,14 +14,95 @@ const command = {
                         .setName("user")
                         .setDescription("The Twitch user")
                         .setMinLength(1)
+                        .setMaxLength(25)
+                        .setRequired(true)
+                )
+                .addStringOption(
+                    new SlashCommandStringOption()
+                        .setName("comment")
+                        .setDescription("The pre-defined comment to add to the user")
+                        .setMinLength(1)
+                        .setMaxLength(5)
+                )
+                .addStringOption(
+                    new SlashCommandStringOption()
+                        .setName("customcomment")
+                        .setDescription("The custom comment to add to the user")
+                        .setMinLength(6)
+                        .setMaxLength(512)
+                )
+        )
+        .addSubcommand(
+            new SlashCommandSubcommandBuilder()
+                .setName("delete")
+                .setDescription("Deletes a Mod Comment under a user")
+                .addStringOption(
+                    new SlashCommandStringOption()
+                        .setName("user")
+                        .setDescription("The Twitch user")
+                        .setMinLength(1)
+                        .setMaxLength(25)
+                        .setRequired(true)
+                )
+                .addStringOption(
+                    new SlashCommandStringOption()
+                        .setName("comment")
+                        .setDescription("The comment to remove from the user")
+                        .setMinLength(1)
+                        .setMaxLength(5)
+                )
+        )
+        .addSubcommand(
+            new SlashCommandSubcommandBuilder()
+                .setName("view")
+                .setDescription("Views Mod Comments under a user")
+                .addStringOption(
+                    new SlashCommandStringOption()
+                        .setName("user")
+                        .setDescription("The Twitch user")
+                        .setMinLength(1)
+                        .setMaxLength(25)
+                        .setRequired(true)
                 )
         ),
     /**
      * Execution function for this command
      * @param {ChatInputCommandInteraction} interaction 
      */
-    execute(interaction) {
-        
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand(true);
+
+        try {
+            let user = await api.Twitch.getUserByName(interaction.options.getString("user", true));
+
+            if (user.length > 0) {
+                user = user[0];
+            } else {
+                interaction.error("User was not found!");
+                return;
+            }
+
+            if (subcommand === "create") {
+                let comment = interaction.options.getString("comment");
+                let customComment = interaction.options.getString("customcomment");
+
+                if (comment) {
+                    
+                } else if (customComment) {
+
+                } else {
+                    interaction.error("...no comment")
+                }
+            } else if (subcommand === "delete") {
+    
+            } else if (subcommand === "view") {
+    
+            } else {
+                interaction.error("Unknown subcommand");
+            }
+        } catch(err) {
+            interaction.error(err);
+        }
     }
 };
 
