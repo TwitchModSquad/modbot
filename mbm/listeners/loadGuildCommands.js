02a11112d3a2d1803984c533fda91b76c2699301
Tyler Twining <twijn@twijn.net>
2023-05-01 15:32:25 -0400

    Temporary command flush

--- mbm/listeners/loadGuildCommands.js
+++ mbm/listeners/loadGuildCommands.js
@@ -55,6 +55,9 @@ const listener = {
                 }, global.api.Logger.warning);
     
                 await guild.commands.fetch();
+                guild.commands.cache.forEach(x => {
+                    guild.commands.delete(x).catch(console.error);
+                })
     
                 Discord.getGuild(guild.id).then(dGuild => {
                     guild.members.cache.forEach(member => {
