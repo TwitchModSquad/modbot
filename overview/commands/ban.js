00c183de495413295013e56f32bbc2fcfd5b122b
Tyler Twining <twijn@twijn.net>
2023-05-18 20:07:41 -0400

    add !ban command

---
+++ overview/commands/ban.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "ban",
+    description: "BOP BOP BOP",
+    async execute(streamer, chatter, tags, args, message) {
+        global.client.overview.say("twitchmodsquad", `BOP BOP BOP`);
+    },
+};
