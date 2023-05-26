0dcadf9c45c6027019101acfa28ff7e1f7c60a87
Tyler Twining <twijn@twijn.net>
2023-05-26 10:43:15 -0400

    Add /signon/verify endpoint

--- web/controllers/signon/index.js
+++ web/controllers/signon/index.js
@@ -8,12 +8,21 @@ router.get("/", (req, res) => {
     }
 
     if (req.authCode >= 2) {
-
+        data.identity = req.session.identity;
         data.twitchAccounts = req.session.identity.twitchAccounts;
         data.discordAccounts = req.session.identity.discordAccounts;
     }
 
     res.render("pages/signon/index", data);
+});
+
+router.get("/verify", async (req, res) => {
+    if (req.authCode >= 2) {
+        const streamers = await req.session.identity.getAllStreamers();
+        res.render("pages/signon/verify", {identity: req.session.identity, streamers: streamers});
+    } else {
+        res.redirect("/signon/");
+    }
 });
 
 module.exports = router;
