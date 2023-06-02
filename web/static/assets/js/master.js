00e5f70785b8785d1a48b2c91fd2b9858170c703
Tyler Twining <twijn@twijn.net>
2023-06-02 13:04:08 -0400

    Add temporary code for API_URI for development use

--- web/static/assets/js/master.js
+++ web/static/assets/js/master.js
@@ -1,3 +1,4 @@
+// const API_URI = "http://localhost:8080/api/"; // developer api uri
 const API_URI = "https://tms.to/api/";
 const DB_URI = "https://db.tms.to/";
 const DB_API_URI = DB_URI + "api/";
