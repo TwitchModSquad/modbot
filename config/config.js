e324301acd9b0c8de949b8e02446b6657104128f
Tyler Twining <twijn@twijn.net>
2025-05-01 12:49:17 -0500

    Change dialect to mariadb

--- config/config.js
+++ config/config.js
@@ -13,13 +13,13 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mariadb"
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mariadb"
   }
 }
