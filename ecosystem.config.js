61f13f21b99582986aa9e9475946c6b56cec9e48
Tyler Twining <twijn@twijn.net>
2025-05-01 13:07:06 -0500

    Remove clustering

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,16 +16,12 @@ module.exports = {
             name: 'web-public',
             script: 'npm',
             args: 'run start:web-public',
-            instances: 2,
-            exec_mode: "cluster",
             env: { NODE_ENV: 'production', PORT: 3010, }
         },
         {
             name: 'web-api',
             script: 'npm',
             args: 'run start:web-api',
-            instances: 2,
-            exec_mode: "cluster",
             env: { NODE_ENV: 'production', PORT: 3020, }
         }
     ]
