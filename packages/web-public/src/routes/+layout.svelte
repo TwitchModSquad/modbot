354edffcdbd05292d063c01d85806d24cd6499c2
Tyler Twining <twijn@twijn.net>
2025-05-01 10:44:45 -0500

    Move assets to cdn.modsquad.tools

---
+++ packages/web-public/src/routes/+layout.svelte
@@ -0,0 +1,4 @@
+<header>
+    <img src="https://cdn.modsquad.tools/assets/images/logo.webp" alt="The Mod Squad logo">
+</header>
+<slot />
--- packages/web-public/svelte.config.js
+++ packages/web-public/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,11 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
 };
 
 export default config;
