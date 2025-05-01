0b7e68e9096bf336471c4b96d1134554cbfd376c
Tyler Twining <twijn@twijn.net>
2025-05-01 12:45:08 -0500

    Add ecosystem.config.js for pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    apps: [
+        {
+            name: 'twitch-bot',
+            script: 'npm',
+            args: 'run start:twitch',
+            env: { NODE_ENV: 'production' }
+        },
+        {
+            name: 'discord-bot',
+            script: 'npm',
+            args: 'run start:discord',
+            env: { NODE_ENV: 'production' }
+        },
+        {
+            name: 'web-public',
+            script: 'npm',
+            args: 'run start:web-public',
+            instances: 2,
+            exec_mode: 'cluster',
+            env: { NODE_ENV: 'production' }
+        },
+        {
+            name: 'web-api',
+            script: 'npm',
+            args: 'run start:web-api',
+            instances: 2,
+            exec_mode: 'cluster',
+            env: { NODE_ENV: 'production' }
+        }
+    ]
+};
