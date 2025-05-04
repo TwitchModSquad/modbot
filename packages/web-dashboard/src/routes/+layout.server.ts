20fc452aee374dfeff3ab3fd877ec57f1f1a6cde
Tyler Twining <twijn@twijn.net>
2025-05-03 21:04:03 -0500

    feat: Basic auth setup for dashboard

---
+++ packages/web-dashboard/src/routes/+layout.server.ts
@@ -0,0 +1,34 @@
+import type { LayoutServerLoad } from './$types';
+import { API_URI } from "$env/static/private";
+import type {RawDiscordUser, RawIdentity, RawTwitchUser} from "@modbot/utils";
+import {redirect} from "@sveltejs/kit";
+
+export const load: LayoutServerLoad = async ({ fetch, cookies }) => {
+    const res = await fetch(`${API_URI}identity/me`, {
+        headers: {
+            Cookie: cookies.getAll().map(c => `${c.name}=${c.value}`).join('; ')
+        }
+    });
+
+
+
+    let identity = null;
+    let twitchUsers = null;
+    let discordUsers = null;
+    if (res.ok) {
+        const data = await res.json();
+        identity = data.data.identity as RawIdentity;
+        twitchUsers = data.data.users.twitch as RawTwitchUser[];
+        discordUsers = data.data.users.discord as RawDiscordUser[];
+    }
+
+    if (!identity || !twitchUsers || !discordUsers) {
+        throw redirect(303, `${API_URI}auth/twitch`);
+    }
+
+    return {
+        identity,
+        twitchUsers,
+        discordUsers,
+    };
+};
--- packages/web-dashboard/src/routes/+page.svelte
+++ packages/web-dashboard/src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+    import type {RawTwitchUser} from "@modbot/utils";
+
+    const { data } = $props();
+
+    const { twitchUsers }: {
+        twitchUsers: RawTwitchUser[];
+    } = data;
+</script>
+
+{#each twitchUsers as user}
+    {user.display_name}
+{/each}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
